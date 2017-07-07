7107d906293f4484d1c5d69be019b83da5e31088
Osfalt <osfalt4@gmail.com>
2017-07-08 02:26:44 +0300

    Сверстал экран текущей погоды

--- OpenWeather/CurrentWeatherViewController.swift
+++ OpenWeather/CurrentWeatherViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class CurrentWeatherViewController: UIViewController {
-
+    
+    @IBOutlet weak var flagImageView: UIImageView!
+    @IBOutlet weak var regionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var windArrowImageView: UIImageView!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var dawnTimeLabel: UILabel!
+    @IBOutlet weak var duskTimeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- OpenWeather/TabBarViewController.swift
+++ OpenWeather/TabBarViewController.swift
@@ -13,32 +13,6 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        initTabBar()
     }
     
-    /*private func initTabBar() {
-        // tab title color
-//        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.5)], for: .normal)
-//        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 1.0)], for: .selected)
-        
-        // tabs
-        let currentWeatherTab: UITabBarItem = tabBar.items![0]
-        currentWeatherTab.selectedImage = UIImage(named: "partly_cloudy.png")?.withRenderingMode(.alwaysOriginal)
-//        currentWeatherTab.image = UIImage(named: "party_cloudy")?.withAlpha(0.5)?.withRenderingMode(.alwaysOriginal)
-        currentWeatherTab.image = UIImage(named: "partly_cloudy.png")?.withRenderingMode(.alwaysOriginal)
-        
-        let forecastTab: UITabBarItem = tabBar.items![1]
-        forecastTab.selectedImage = UIImage(named: "calendar.png")?.withRenderingMode(.alwaysOriginal)
-//        forecastTab.image = UIImage(named: "tab_search_doctor")?.withAlpha(0.5)?.withRenderingMode(.alwaysOriginal)
-        forecastTab.image = UIImage(named: "calendar.png")?.withRenderingMode(.alwaysOriginal)
-        
-        let profileTab: UITabBarItem = tabBar.items![2]
-//        profileTab.selectedImage = UIImage(named: "tab_profile")?.withRenderingMode(.alwaysOriginal)
-//        profileTab.image = UIImage(named: "tab_profile")?.withAlpha(0.5)?.withRenderingMode(.alwaysOriginal)
-        
-        selectedIndex = 0
-        
-        // delegate
-//        delegate = self
-    }*/
 }
